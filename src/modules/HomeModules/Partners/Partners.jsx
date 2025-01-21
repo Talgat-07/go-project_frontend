a83c379a3f2330823f6060fa9cd438f42624eef6
dav <703-dav@users.noreply.gitlab.geeks.kg>
2025-01-21 21:30:53 +0600

    refactor: refactor partners section

--- src/modules/HomeModules/Partners/Partners.jsx
+++ src/modules/HomeModules/Partners/Partners.jsx
@@ -1,25 +1,58 @@
-import { Typography } from '@/ui/Typography/Typography'
-import styles from './Partners.module.scss'
-import { SwitchButton } from '@/ui/SwitchButton/SwitchButton'
-import { PartnersApi } from './api/PartnersApi'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { Typography } from '@/ui/Typography/Typography'
+import { SwitchButton } from '@/ui/SwitchButton/SwitchButton'
+import { PartnersApi } from './api/PartnersApi'
+import styles from './Partners.module.scss'
 
 export const Partners = () => {
   const { partnersData, partnersRequest } = PartnersApi()
 
   useEffect(() => {
     partnersRequest()
-    console.log(partnersData)
   }, [partnersRequest])
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  }
 
   return (
     <section className={styles.partnersSection}>
-      <Typography variant='h2' className={styles.title}>
+      <Typography variant='h1' weight='fw5' className={styles.title}>
         Наши партнеры
       </Typography>
-      <div>
-        <div className={styles.logos}>
+      <div className={styles.sliderWrapper}>
+        <Slider {...settings}>
           {partnersData.map((item) => (
             <div key={item.id}>
               <Link to={item.link}>
@@ -31,15 +64,15 @@ export const Partners = () => {
               </Link>
             </div>
           ))}
-        </div>
-        <Typography variant='p' className={styles.enticementDescription}>
-          Хотите стать частью команды наших партнеров? Напишите нам и мы обсудим
-          возможности сотрудничества!
-        </Typography>
-        <SwitchButton maxWidth='286px'>
-          Написать на WhatsApp
-        </SwitchButton>
+        </Slider>
       </div>
+      <Typography variant='h4' weight='fw5' className={styles.enticementDescription}>
+        Хотите стать частью команды наших партнеров?<br />
+        Напишите нам и мы обсудим возможности сотрудничества!
+      </Typography>
+      <SwitchButton maxWidth='250px'>
+        Написать на WhatsApp
+      </SwitchButton>
     </section>
   )
 }
