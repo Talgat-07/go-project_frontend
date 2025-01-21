66a5cdbb833ec3586f77165c1ffe62e12a3a99f5
gdegjygr <malcevartem808@gmail.com>
2025-01-21 20:17:26 +0600

    .

--- src/utils/requester/requester.js
+++ src/utils/requester/requester.js
@@ -1,13 +1,11 @@
-import axios from 'axios';
-import { BASE_URL } from 'utils/constants/constants';
+import axios from 'axios'
+import { BASE_URL } from 'utils/constants/constants'
 
 export const requester = axios.create({
   baseURL: BASE_URL + 'api/v1/',
-});
+})
 
 requester.interceptors.request.use((config) => {
-  const language = JSON.parse(localStorage.getItem('language'))?.code || 'ru';
-  config.headers['Accept-Language'] = language;
-
-  return config;
-});
+  const language = JSON.parse(localStorage.getItem('language'))?.code || 'ru'
+  if ((config.headers['Accept-Language'] = language)) return config
+})
