25127fe5578361a70b188c0285ff9b601d2e1191
dav <703-dav@users.noreply.gitlab.geeks.kg>
2025-01-25 03:27:01 +0600

    fix

--- src/ui/TourCard/TourCard.jsx
+++ src/ui/TourCard/TourCard.jsx
@@ -6,31 +6,31 @@ import styles from './TourCard.module.scss'
 
 export const TourCard = ({ item }) => {
   return (
-    <Link to={`/tours/${item.id}`}>
+    <div
+      className={styles.card}
+      style={{ backgroundImage: `url(${item.image})` }}
+    >
       <div
-        className={styles.card}
-        style={{ backgroundImage: `url(${item.image})` }}
+        className={styles.cardContent}
+        style={{ background: tourCardColors[item.background_color] }}
       >
-        <div
-          className={styles.cardContent}
-          style={{ background: tourCardColors[item.background_color] }}
-        >
-          <Typography variant='fs24' weight='fw7' useParser>
-            {item.description}
-          </Typography>
-          {/* <Typography weight='fw5' className={styles.date}>
+        <Typography variant='fs24' weight='fw7' useParser>
+          {item.description}
+        </Typography>
+        {/* <Typography weight='fw5' className={styles.date}>
           {item?.mini_info_frames.tour_date}
         </Typography> */}
-          <div className={styles.price}>
-            {/* <Typography weight='fw7'>
+        <div className={styles.price}>
+          {/* <Typography weight='fw7'>
             {item?.mini_info_frames.price}
           </Typography> */}
+          <Link to={`/tours/${item.id}`}>
             <SwitchButton maxWidth='165px'>
               Подробнее
             </SwitchButton>
-          </div>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
