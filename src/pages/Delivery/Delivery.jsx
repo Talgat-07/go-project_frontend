0022db090529fd08f94db3d2d8b31939b8ff18ff
Kyial <mackyial05@gmail.com>
2025-01-29 19:03:36 +0600

    fix

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -1,10 +1,21 @@
-import styles from './Delivery.module.scss'
-import DeliveryModule from '@/modules/DeliveryModules/DeliveryModule'
+import styles from './Delivery.module.scss';
+import DeliveryModule from '@/modules/DeliveryModules/DeliveryModule';
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export const Delivery = () => {
   return (
-    <main className={styles.deliveryPage}>
-      <DeliveryModule />
-    </main>
-  )
-}
+    <>
+      <HelmetProvider>
+        <Helmet prioritizeSeoTags>
+          <title>Доставка товаров</title>
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+          <meta property="og:title" content="Доставка товаров" />
+          <meta property="og:description" content="Доставка товаров" />
+        </Helmet>
+      </HelmetProvider>
+      <main className={styles.deliveryPage}>
+        <DeliveryModule />
+      </main></>
+  );
+};
