bd0e5e1651339908f5280d7cc316c6b9b7fe4d69
Kyial <mackyial05@gmail.com>
2025-02-04 12:53:34 +0600

    artems fixes

--- src/modules/HomeModules/components/Banner/Banner.jsx
+++ src/modules/HomeModules/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import styles from './Banner.module.scss'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -10,13 +10,36 @@ import { ToursSliderApi } from '../../api/ToursSliderApi'
 export const Banner = () => {
   const { toursData, toursRequest } = ToursSliderApi()
 
+  const swiperRef = useRef(null)
+
+  const customStyles = `
+    .custom-swiper .swiper-pagination-bullet {
+      width: 25px;
+      height: 25px;
+      border-radius: 100%;
+      background: #c3bebe;
+      opacity: 0.72;
+    }
+
+    .custom-swiper .swiper-pagination-bullet.swiper-pagination-bullet-active {
+      background: #fff;
+      opacity: 1;
+    }
+  `
+
+  const styleSheet = document.createElement('style')
+  styleSheet.type = 'text/css'
+  styleSheet.innerText = customStyles
+  document.head.appendChild(styleSheet)
+
   useEffect(() => {
     toursRequest()
   }, [toursRequest])
 
   return (
     <Swiper
-      className={styles.slider}
+      ref={swiperRef}
+      className={`${styles.slider} custom-swiper`}
       modules={[Pagination, Autoplay, Zoom]}
       pagination={{
         clickable: true,
@@ -25,7 +48,7 @@ export const Banner = () => {
         crossFade: true,
       }}
       loop={true}
-      speed={1000}
+      speed={1150}
       easing={'ease-in-out'}
       autoplay={{
         delay: 13000,
