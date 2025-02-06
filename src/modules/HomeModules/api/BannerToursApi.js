b487fcfc3428a08001edfbd1a3de43789864ba99
Kyial <mackyial05@gmail.com>
2025-02-06 16:03:05 +0600

    fx

---
+++ src/modules/HomeModules/api/BannerToursApi.js
@@ -0,0 +1,12 @@
+import { requester } from '@/utils/requester/requester'
+import { create } from 'zustand'
+
+const endpoint = 'tours/main-tours/'
+
+export const BannerTuorsApi = create((set) => ({
+  toursData: [],
+  toursRequest: async () => {
+    const response = await requester.get(endpoint)
+    set({ toursData: response.data })
+  },
+}))
